67ae3b6d73e643fabcace7898c6cb5b547b73886
Yanan Guo <gynsolomon@gmail.com>
2015-08-21 17:22:52 +0800

    uncomment wechat api response

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-//var wechat = require('wechat');
+var wechat = require('wechat');
 
 /* GET wechat api test. */
-//router.get('/', wechat('solomon',function(req, res){
-//
-//}));
+router.get('/', wechat('solomon',function(req, res){
+    res.sendStatus(200);
+}));
 
 module.exports = router;
