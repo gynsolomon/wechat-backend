24b1c3395fd60c4e08fb63b610f3bb8565c0aa32
Yanan Guo <gynsolomon@gmail.com>
2015-08-21 16:43:13 +0800

    init commit

---
+++ routes/wechat.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+//var wechat = require('wechat');
+
+/* GET wechat api test. */
+//router.get('/', wechat('solomon',function(req, res){
+//
+//}));
+
+module.exports = router;
